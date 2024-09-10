ab3c5522d5b1d61f47ca679a0ba6e9fab0150842
Tiago do Nascimento Silva <tiago_silvaa10@outlook.com>
2024-09-10 17:08:02 -0300

    adicionando a verificação de perfis no sistema

--- valida_login.php
+++ valida_login.php
@@ -3,17 +3,25 @@
 
     // variavel que verifica se a autenticação foi realizada
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = array(1 => 'Administrativo', 2 => 'Usuario');
 
     // usuarios do sistema
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => '123'),
+        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1),
+        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '123', 'perfil_id' => 1),
+        array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+        array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '12345', 'perfil_id' => 2),
     );
 
     foreach($usuarios_app as $user) {
 
         if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
             $_SESSION['usuario'] = $user['email'];  // Armazena o e-mail do usuário na sessão
             break;  // Interrompe o loop após encontrar o usuário
         }
@@ -21,6 +29,8 @@
 
     if($usuario_autenticado) {
         $_SESSION['autenticado'] = true;
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
         header('Location: home.php');
         exit; // Garante que o script não continue executando
     } else {
